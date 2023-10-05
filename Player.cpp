cfc876027d0d3716e2971f2a06f9c447cf7e239d
kura495 <104417866+kura495@users.noreply.github.com>
2023-10-05 12:21:06 +0900

    add Function Player.cpp/h

--- Player.cpp
+++ Player.cpp
@@ -11,9 +11,41 @@ void Player::Initalize() {
 }
 
 void Player::Update() { 
+
+	if (behaviorRequest_) {
+		behavior_ = behaviorRequest_.value();
+		switch (behavior_) {
+		case Behavior::kRoot:
+		default:
+			BehaviorRootUpdate();
+		case Behavior::kJump:
+			BehaviorJumpUpdate();
+		break;
+		case Behavior::kDrop:
+			BehaviorDropUpdate();
+		break;
+		}
+		behaviorRequest_ = std::nullopt;
+	}
+
+
 	worldTransform_.UpdateMatrix();
 }
 
 void Player::Draw(const ViewProjection& ViewProjection_) { 
 	model_.get()->Draw(worldTransform_, ViewProjection_);
 }
+
+void Player::BehaviorRootUpdate() {
+
+}
+
+void Player::BehaviorJumpUpdate() {
+
+}
+
+void Player::BehaviorDropUpdate() {
+
+}
+
+
--- Player.h
+++ Player.h
@@ -1,9 +1,16 @@
 ﻿#pragma once
-
+//
+#include <optional>
+//kamataEngine
 #include "Model.h"
-
 #include "WorldTransform.h"
 #include "ViewProjection.h"
+
+enum class Behavior {
+	kRoot,//通常
+	kJump,//ジャンプ(単押し)
+	kDrop,//落下攻撃(長押し)
+};
 
 class Player {
 public:
@@ -15,7 +22,17 @@ public:
 	void Draw(const ViewProjection& ViewProjection_);
 
 private:
-	std::unique_ptr<Model> model_ = nullptr;
 
+	void BehaviorRootUpdate();
+	void BehaviorJumpUpdate();
+	void BehaviorDropUpdate();
+
+	//kamataEngine
+	std::unique_ptr<Model> model_ = nullptr;
 	WorldTransform worldTransform_;
+
+	//
+	Behavior behavior_ = Behavior::kRoot;
+	std::optional<Behavior> behaviorRequest_ = std::nullopt;
+
 };
